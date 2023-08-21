2db0c09e5d27bde7f5c04f8c13c05820252b3011
Matthias Nowak <39731988+matthias-p-nowak@users.noreply.github.com>
2023-08-21 07:39:05 +0200

    Update build-pack-upload.ps1

--- CliUi/build-pack-upload.ps1
+++ CliUi/build-pack-upload.ps1
@@ -1,4 +1,4 @@
 remove-item -recurse bin
 remove-item -recurse obj
 dotnet build -c Release
-dotnet nuget push  .\bin\Release\CliUi.*.nupkg -k oy2hiais73dbgrlqz4pqiszepc4ip5ouhaqd3mpd5h4uca --source https://api.nuget.org/v3/index.json --skip-duplicate
+dotnet nuget push  .\bin\Release\CliUi.*.nupkg --source https://api.nuget.org/v3/index.json --skip-duplicate

3cf754460e59d9e8be823c9d0dc2e5ab326a97b9
Matthias Nowak <39731988+matthias-p-nowak@users.noreply.github.com>
2023-08-21 07:39:33 +0200

    Delete build-pack-upload.ps1
    exposed key

--- CliUi/build-pack-upload.ps1
+++
@@ -1,4 +0,0 @@
-remove-item -recurse bin
-remove-item -recurse obj
-dotnet build -c Release
-dotnet nuget push  .\bin\Release\CliUi.*.nupkg --source https://api.nuget.org/v3/index.json --skip-duplicate
